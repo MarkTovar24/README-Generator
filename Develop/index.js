d39957b1c863bd77032fc8c1113dcdda98044486
Mark Tovar <markjtovar24@gmail.com>
2024-02-21 19:40:21 -0600

    added question array and installed inquierer

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,51 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your README Title: ",
+    
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a brief description of your application:",
+    },
+    {
+        type: "input",
+        name: "installinfo",
+        message: "Please enter information on how to install your Application:",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter information on how to use your Application:",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Enter Contribution Guidlines:",
+    },
+    {
+        type: "input",
+        name: "testing",
+        message: "Enter testing instructions",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Choose your License:",
+        choices: [
+            "MIT",
+            "GPL 3.0",
+            "BSD 3",
+            "None",
+        ],
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
